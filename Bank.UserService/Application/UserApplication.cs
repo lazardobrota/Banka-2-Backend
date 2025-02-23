668a49a69c015a98423a290856b2f3bf8807f5e6
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2025-02-23 22:16:44 +0100

    Project Setup

---
+++ Bank.UserService/Application/UserApplication.cs
@@ -0,0 +1,27 @@
+﻿namespace Bank.UserService.Application;
+
+public class UserApplication
+{
+    public static void Run(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+    
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
