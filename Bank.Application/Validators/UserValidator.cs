22ce3aeff68d684bb348202d7b450f27472adbe5
Dušan Čolić <dcolic3121rn@raf.rs>
2025-02-24 14:35:45 +0100

    Implement endpoint for user login

---
+++ Bank.Application/Validators/UserValidator.cs
@@ -0,0 +1,20 @@
+﻿using Bank.Application.Requests;
+
+using FluentValidation;
+
+namespace Bank.Application.Validators;
+
+public static class UserValidator
+{
+    public class Login : AbstractValidator<UserLoginRequest>
+    {
+        public Login()
+        {
+            RuleFor(userRequest => userRequest.Email)
+            .NotEmpty()
+            .WithMessage("Email is required.")
+            .EmailAddress()
+            .WithMessage("Email is invalid.");
+        }
+    }
+}
