805b7e0b77002b25b4435beeae0bca0df9ab6f43
lazardobrota <lazardobrota1323@gmail.com>
2025-02-25 19:32:35 +0100

    Feat: Add GetAll Employees

--- Bank.UserService/Services/EmployeeService.cs
+++ Bank.UserService/Services/EmployeeService.cs
@@ -1,4 +1,6 @@
-﻿using Bank.Application.Endpoints;
+﻿using Bank.Application.Domain;
+using Bank.Application.Endpoints;
+using Bank.Application.Queries;
 using Bank.Application.Requests;
 using Bank.Application.Responses;
 using Bank.UserService.Mappers;
@@ -8,17 +10,31 @@ namespace Bank.UserService.Services;
 
 public interface IEmployeeService
 {
+    Task<Result<List<EmployeeResponse>>> GetAll(UserFilterQuery userFilterQuery, Pageable pageable);
     Task<Result<EmployeeResponse>> GetOne(Guid id);
 
     Task<Result<EmployeeResponse>> Create(EmployeeCreateRequest employeeCreateRequest);
 
     Task<Result<EmployeeResponse>> Update(EmployeeUpdateRequest employeeUpdateRequest, Guid id);
+    
 }
 
 public class EmployeeService(IUserRepository userRepository) : IEmployeeService
 {
     private readonly IUserRepository m_UserRepository = userRepository;
 
+    public async Task<Result<List<EmployeeResponse>>> GetAll(UserFilterQuery userFilterQuery, Pageable pageable)
+    {
+        var users = await m_UserRepository.FindAll(userFilterQuery, pageable);
+
+        if (users.Count == 0)
+            return Result.NoContent<List<EmployeeResponse>>();
+
+        return Result.Ok(users.Select(user => user.ToEmployee()
+                                                  .ToResponse())
+                              .ToList());
+    }
+    
     public async Task<Result<EmployeeResponse>> GetOne(Guid id)
     {
         var user = await m_UserRepository.FindById(id);
