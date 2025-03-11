2682a4780d1515e1104d6a158f5599164a4ebe55
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2025-03-11 18:40:01 +0100

    Add Request Validation for Account entities

---
+++ Bank.Application/Requests/AccountTypeRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Bank.Application.Requests;
+
+public class AccountTypeCreateRequest
+{
+    public required string Name { set; get; }
+    public required string Code { set; get; }
+}
+
+public class AccountTypeUpdateRequest
+{
+    public required string Name { set; get; }
+    public required string Code { set; get; }
+}
