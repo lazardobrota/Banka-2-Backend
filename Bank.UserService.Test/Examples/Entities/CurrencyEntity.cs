ae1f698b87a347d63ea3e90d8779b4fd09aa7388
Lana Lazarević <116087215+LanaLazarevic@users.noreply.github.com>
2025-03-19 17:10:32 +0100

    Add Integration test for Country and Currency

---
+++ Bank.UserService.Test/Examples/Entities/CurrencyEntity.cs
@@ -0,0 +1,24 @@
+﻿using Bank.Application.Queries;
+using Bank.UserService.Database.Seeders;
+
+namespace Bank.UserService.Test.Examples.Entities;
+
+public static partial class Example
+{
+    public static partial class Entity
+    {
+        public static class Currency
+        {
+            public static readonly CurrencyFilterQuery FilterQueryWithName = new()
+                                                                         {
+                                                                             Name                  = "Euro",
+                                                                         };
+            public static readonly CurrencyFilterQuery FilterQueryWithCode = new()
+                                                                             {
+                                                                                 Code                  = "EUR",
+                                                                             };
+
+            public static readonly Guid GetById = Seeder.Currency.AustralianDollar.Id;
+        }
+    }
+}
---
+++ Bank.UserService.Test/Steps/CurrencySteps.cs
@@ -0,0 +1,90 @@
+﻿using System.Reflection.Metadata;
+
+using Bank.Application.Endpoints;
+using Bank.Application.Queries;
+using Bank.Application.Responses;
+using Bank.UserService.Services;
+using Bank.UserService.Test.Examples.Entities;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Shouldly;
+
+namespace Bank.UserService.Test.Steps;
+[Binding]
+public class CurrencySteps(ScenarioContext scenarioContext, ICurrencyService currencyService)
+{
+    private readonly ICurrencyService m_CurrencyService = currencyService;
+    private readonly ScenarioContext  m_ScenarioContext = scenarioContext;
+    
+    [Given(@"currency get request with name filter parameter")]
+    public void GivenCurrencyGetRequestWithNameFilterParameter()
+    {
+        m_ScenarioContext[Constant.FilterParam] = Example.Entity.Currency.FilterQueryWithName;
+    }
+
+    [Given(@"currency get request with code filter parameter")]
+    public void GivenCurrencyGetRequestWithCodeFilterParameter()
+    {
+        m_ScenarioContext[Constant.FilterParam] = Example.Entity.Currency.FilterQueryWithCode;
+    }
+    
+    [Given(@"currency get request with Id")]
+    public void GivenCurrencyGetRequestWithId()
+    {
+        m_ScenarioContext[Constant.Id] = Example.Entity.Currency.GetById;
+    }
+
+    [When(@"currencies are fetched from the database")]
+    public async Task WhenCurrenciesAreFetchedFromTheDatabase()
+    {
+        var filterQuery = m_ScenarioContext.Get<CurrencyFilterQuery>(Constant.FilterParam);
+        var result      = await m_CurrencyService.FindAll(filterQuery);
+        m_ScenarioContext[Constant.GetResult] = result;
+    }
+    
+    [When(@"currency is fetched by Id from the database")]
+    public async Task WhenCurrencyIsFetchedByIdFromTheDatabase()
+    {
+        var id = m_ScenarioContext.Get<Guid>(Constant.Id);
+        var result      = await m_CurrencyService.FindById(id);
+        m_ScenarioContext[Constant.GetResult] = result;
+    }
+
+    [Then(@"response should contain a list of currencies matching the name filter")]
+    public void ThenResponseShouldContainAListOfCurrenciesMatchingTheNameFilter()
+    {
+        var name = m_ScenarioContext.Get<CurrencyFilterQuery>(Constant.FilterParam).Name;
+        var result = m_ScenarioContext.Get<Result<List<CurrencyResponse>>>(Constant.GetResult);
+        result.ActionResult.ShouldBeOfType<OkObjectResult>();
+        result.Value.ShouldNotBeNull();
+        name.ShouldNotBeNull();
+        result.Value.ShouldAllBe(c => c.Name.Contains(name));
+    }
+
+    [Then(@"response should contain a list of currencies matching the code filter")]
+    public void ThenResponseShouldContainAListOfCurrenciesMatchingTheCodeFilter()
+    {
+        var code = m_ScenarioContext.Get<CurrencyFilterQuery>(Constant.FilterParam).Code;
+        var result = m_ScenarioContext.Get<Result<List<CurrencyResponse>>>(Constant.GetResult);
+        result.ActionResult.ShouldBeOfType<OkObjectResult>();
+        result.Value.ShouldNotBeNull();
+        code.ShouldNotBeNull();
+        result.Value.ShouldAllBe(c => c.Code == code);
+    }
+    
+    [Then(@"response should contain the currency with the given Id")]
+    public void ThenResponseShouldContainTheCurrencyWithTheGivenId()
+    {
+        var result = m_ScenarioContext.Get<Result<CurrencyResponse>>(Constant.GetResult);
+        result.ActionResult.ShouldBeOfType<OkObjectResult>();
+        result.Value.ShouldNotBeNull();
+        result.Value.Id.ShouldBe(m_ScenarioContext.Get<Guid>(Constant.Id));
+    }
+}
+file static class Constant
+{
+    public const string FilterParam = "CurrencyFilterQuery";
+    public const string Id          = "CurrencyId";
+    public const string GetResult      = "CurrancyGetResult";
+}
