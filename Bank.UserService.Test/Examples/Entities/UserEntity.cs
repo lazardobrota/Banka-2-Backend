8e3a493f052cc94b6d0a0a5f31222a87ede2c98a
lazardobrota <lazardobrota1323@gmail.com>
2025-03-21 00:42:02 +0100

    Add integration tests for User

--- Bank.UserService.Test/Examples/Entities/UserEntity.cs
+++ Bank.UserService.Test/Examples/Entities/UserEntity.cs
@@ -1,6 +1,10 @@
 ﻿using Bank.Application.Requests;
+using Bank.UserService.Database.Seeders;
+using Bank.UserService.Models;
 
 namespace Bank.UserService.Test.Examples.Entities;
+
+using UserModel = User;
 
 public static partial class Example
 {
@@ -13,6 +17,56 @@ public static partial class Example
                                                                        Email    = "admin@gmail.com",
                                                                        Password = "admin"
                                                                    };
+
+            public static readonly UserModel GetEmployee = new()
+                                                           {
+                                                               Id                         = Seeder.Employee.Employee01.Id,
+                                                               FirstName                  = Seeder.Employee.Employee01.FirstName,
+                                                               LastName                   = Seeder.Employee.Employee01.LastName,
+                                                               DateOfBirth                = Seeder.Employee.Employee01.DateOfBirth,
+                                                               Gender                     = Seeder.Employee.Employee01.Gender,
+                                                               UniqueIdentificationNumber = Seeder.Employee.Employee01.UniqueIdentificationNumber,
+                                                               Email                      = Seeder.Employee.Employee01.Email,
+                                                               Username                   = Seeder.Employee.Employee01.Username,
+                                                               PhoneNumber                = Seeder.Employee.Employee01.PhoneNumber,
+                                                               Address                    = Seeder.Employee.Employee01.Address,
+                                                               Password                   = Seeder.Employee.Employee01.Password,
+                                                               Salt                       = Seeder.Employee.Employee01.Salt,
+                                                               Role                       = Seeder.Employee.Employee01.Role,
+                                                               Department                 = Seeder.Employee.Employee01.Department,
+                                                               CreatedAt                  = Seeder.Employee.Employee01.CreatedAt,
+                                                               ModifiedAt                 = Seeder.Employee.Employee01.ModifiedAt,
+                                                               Employed                   = Seeder.Employee.Employee01.Employed,
+                                                               Activated                  = Seeder.Employee.Employee01.Activated
+                                                           };
+
+            public static readonly UserModel UpdateEmployee = new()
+                                                              {
+                                                                  Id                         = Seeder.Employee.Employee02.Id,
+                                                                  FirstName                  = Seeder.Employee.Employee02.FirstName,
+                                                                  LastName                   = Seeder.Employee.Employee02.LastName,
+                                                                  DateOfBirth                = Seeder.Employee.Employee02.DateOfBirth,
+                                                                  Gender                     = Seeder.Employee.Employee02.Gender,
+                                                                  UniqueIdentificationNumber = Seeder.Employee.Employee02.UniqueIdentificationNumber,
+                                                                  Email                      = Seeder.Employee.Employee02.Email,
+                                                                  Username                   = Seeder.Employee.Employee02.Username,
+                                                                  PhoneNumber                = Seeder.Employee.Employee02.PhoneNumber,
+                                                                  Address                    = Seeder.Employee.Employee02.Address,
+                                                                  Password                   = Seeder.Employee.Employee02.Password,
+                                                                  Salt                       = Seeder.Employee.Employee02.Salt,
+                                                                  Role                       = Seeder.Employee.Employee02.Role,
+                                                                  Department                 = Seeder.Employee.Employee02.Department,
+                                                                  CreatedAt                  = Seeder.Employee.Employee02.CreatedAt,
+                                                                  ModifiedAt                 = Seeder.Employee.Employee02.ModifiedAt,
+                                                                  Employed                   = Seeder.Employee.Employee02.Employed,
+                                                                  Activated                  = Seeder.Employee.Employee02.Activated
+                                                              };
+
+            public static readonly UserActivationRequest UserActivationRequest = new()
+                                                                                 {
+                                                                                     Password        = "em12345678",
+                                                                                     ConfirmPassword = "em12345678",
+                                                                                 };
         }
     }
 }
