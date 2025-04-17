a5da569ec3947d99b57afc9458034bead9fac866
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2025-04-17 04:32:22 +0200

    Http | Add Service Token Environment Variable

---
+++ Bank.Http/Configurations/JwtConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Bank.Application.Utilities;
+
+namespace Bank.Http.Configurations;
+
+public static partial class Configuration
+{
+    public static class Jwt
+    {
+        public static readonly string ServiceToken = EnvironmentUtilities.GetStringVariable("BANK_HTTP_JWT_SERVICE_TOKEN");
+    }
+}
--- Bank.Http/HttpExtensions.cs
+++ Bank.Http/HttpExtensions.cs
@@ -12,8 +12,7 @@ public static class HttpExtensions
         services.AddHttpClient(Configuration.Client.Name.UserService, httpClient =>
                                                                       {
                                                                           httpClient.BaseAddress = new Uri($"{Configuration.Client.BaseUrl.UserService}");
-                                                                          //TODO
-                                                                          //httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjEzMDU5MzY1NzI1NCwiaWQiOiJjNmY0NDEzMy0wOGYyLTRhNDMtYmQ2NS05Y2ZiNmIxM2ZhNWIiLCJwZXJtaXNzaW9uIjoiMiIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTc0NDYzODQzNCwibmJmIjoxNzQ0NjM4NDM0fQ.1cA329l2bWUlENwYrq03l1yQ0Jxw597kw-YUT0WipiI");
+                                                                          httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {Configuration.Jwt.ServiceToken}");
                                                                       });
 
         services.AddSingleton<IUserServiceHttpClient, UserServiceHttpClient>();
