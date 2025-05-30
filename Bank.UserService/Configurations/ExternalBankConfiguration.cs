db822bfa1ce5c1a1b55dd199bc3eef945cd63b6b
Nemanja Radovanovic <nradovanovic12022rn@raf.rs>
2025-05-30 20:27:09 +0200

    User | Add External Bank Configurations

---
+++ Bank.UserService/Configurations/ExternalBankConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Bank.Application.Utilities;
+
+namespace Bank.UserService.Configurations;
+
+public partial class Configuration
+{
+    public static class ExternalBank
+    {
+        public static readonly string Bank1BaseUrl = EnvironmentUtilities.GetStringVariable("BANK_EXTERNAL_BANK_BASE_URL_BANK_1");
+        public static readonly string Bank3BaseUrl = EnvironmentUtilities.GetStringVariable("BANK_EXTERNAL_BANK_BASE_URL_BANK_3");
+        public static readonly string Bank4BaseUrl = EnvironmentUtilities.GetStringVariable("BANK_EXTERNAL_BANK_BASE_URL_BANK_4");
+    }
+}
